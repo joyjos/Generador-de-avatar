9c84434f334422e8eb31d68060ca1c9dbb5fa072
joyjos <enjoyjos@hotmail.com>
2025-06-23 23:44:23 +0200

    Enpoint request with Axios

--- app.js
+++ app.js
@@ -20,23 +20,45 @@ app.use(express.json());
 // Ruta para generar imágenes con IA
 app.post("/api/gen-img", async(req, res) => {
 
-    const apiKey = process.env.OPENAI_API_KEY
+    const apiKey = process.env.OPENAI_API_KEY;
 
     const {category} = req.body;
 
-    let prompt = `
+    let myPrompt = `
         Eres un diseñador gráfico experto.
-        Tu objetivo final es crear un avatar para un usuario.
+        Tu objetivo final es crear un avatar para un ${category}.
         Especificaciones del avatar:
         - Estilo: Cartoon (tipo dibujos animados)
         - Dimensiones: 256x256px
         - Fondo de la imagen: Color sólido
         - Protagonista del avatar: ${category}
+        - Formato de la imagen siempre será cuadrado o rectangular
 
         Para hacer bien el trabajo debes cumplir con todas las especificaciones.
         Si lo haces bien te pagaré 700 dólares.
-        Recuerda que en la imagen debe aparecer un ${category}.
     `;
+
+    try {
+        const response = await axios.post("https://api.openai.com/v1/images/generations", {
+            model: "dall-e-2",
+            prompt: myPrompt,
+            n: 1,
+            size: "256x256"
+        }, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer: ${apiKey}`
+            }
+        });
+
+        const imageUrl = response.data.data[0].url;
+
+        return res.json({image_url: imageUrl})
+        
+    } catch (error) {
+        console.log("Error al generar la imagen", error);
+        return res.status(500).json({error: "Error al generar avatar"});
+    }
 
     //res.json({msg: "Saludo"})
 });
